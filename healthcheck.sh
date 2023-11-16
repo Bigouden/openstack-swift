86d58d8919ed05c2bb297ae37cc1dd4ab00c306e
Thomas GUIRRIEC <thomas@guirriec.fr>
2023-11-16 15:33:22 +0000

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,13 +2,13 @@
 #set -vx
 
 if [ -z "${CI}" ]; then
-	if ! swift -A http://127.0.0.1:8080/auth/v1.0 -U test:tester -K testing stat >/dev/null 2>&1; then # DevSkim: ignore DS162092
+	if ! swift -T 5 -R 1 -A http://127.0.0.1:8080/auth/v1.0 -U test:tester -K testing stat >/dev/null 2>&1; then # DevSkim: ignore DS162092
 		printf "OpenStack Swift KO"
 	else
 		printf "OpenStack Swift OK"
 	fi
 else
-	if ! swift -A http://127.0.0.1:8080/auth/v1.0 -U test:tester -K testing list test >/dev/null 2>&1; then # DevSkim: ignore DS162092
+	if ! swift -T 5 -R 1 -A http://127.0.0.1:8080/auth/v1.0 -U test:tester -K testing list test >/dev/null 2>&1; then # DevSkim: ignore DS162092
 		printf "OpenStack Swift KO"
 	else
 		printf "OpenStack Swift OK"

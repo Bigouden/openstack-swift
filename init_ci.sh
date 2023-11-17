8ba2124c0f70bdb98000c2d632e4ff93cdd70e45
Thomas GUIRRIEC <thomas@guirriec.fr>
2023-11-17 08:52:26 +0000

    Fix output

--- init_ci.sh
+++ init_ci.sh
@@ -7,7 +7,7 @@ export CI=true
 nohup /init &
 
 # Wait For TCP 8080
-while ! nc -vz -w 1 localhost 8080; do # DevSkim: ignore DS162092
+while ! nc -vz -w 1 localhost 8080 >/dev/null 2>&1; do # DevSkim: ignore DS162092
 	sleep 1
 done
 
@@ -15,12 +15,12 @@ done
 printf "TEST" >test.txt
 
 # Create OpenStack Swift "test" Container
-swift -A http://127.0.0.1:8080/auth/v1.0 -U test:tester -K testing post test # DevSkim: ignore DS162092
+swift -A http://127.0.0.1:8080/auth/v1.0 -U test:tester -K testing post test >/dev/null 2>&1 # DevSkim: ignore DS162092
 
 # Upload "test.txt" in OpenStack Swift "test" Container
-swift -A http://127.0.0.1:8080/auth/v1.0 -U test:tester -K testing upload test test.txt # DevSkim: ignore DS162092
+swift -A http://127.0.0.1:8080/auth/v1.0 -U test:tester -K testing upload test test.txt >/dev/null 2>&1 # DevSkim: ignore DS162092
 
-printf "OpenStack Swift start !"
+echo "OpenStack Swift start !"
 
 # Sleep infinity
 sleep infinity
